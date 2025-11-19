37ab25c16d152aea72d4531aecd70e09e789115f
muslimumarov <norbekumarov613@gmail.com>
2025-11-19 11:01:59 +0500

    Birinchi commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+/**
+ * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ */
+(function ($) {
+    "use strict";
+      $('.sakura-falling').sakura();
+})(jQuery);
+
+/**
+ *
+ * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
+ *
+ * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
+ * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
+ * Let's keep the sky-high status of this song untouched!
+ *
+ * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
+ * Choose a heart-touching track! 🎵 ❤️
+ *
+ * Listen here: https://youtu.be/X0MDALpV29s
+ *
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    let audio = document.getElementById("my_audio");
+
+    if (!audio) {
+        console.error("Audio elementi topilmadi!");
+        return;
+    }
+
+    // Sahifa yuklanishi bilan avtomatik boshlash
+    audio.play().catch(error => console.log("Autoplay bloklandi:", error));
+
+    $(document).on("click", function() {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+});
+
+// Set the date we're counting down to
+
+// being a bit cool :p  
+var styles = [
+    'background: linear-gradient(#D33106, #571402)'
+    , 'border: 4px solid #3E0E02'
+    , 'color: white'
+    , 'display: block'
+    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
+    , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'line-height: 40px'
+    , 'text-align: center'
+    , 'font-weight: bold'
+    , 'font-size: 32px'
+].join(';');
+
+var styles1 = [
+    'color: #FF6C37'
+    , 'display: block'
+    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
+    , 'line-height: 40px'
+    , 'font-weight: bold'
+    , 'font-size: 32px'
+].join(';');
+
+var styles2 = [
+    'color: teal'
+    , 'display: block'
+    , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 1)'
+    , 'line-height: 40px'
+    , 'font-weight: bold'
+    , 'font-size: 32px'
+].join(';');
+
+console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+
+console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+
+console.log(
+    `%cShaadi me zaroor aana!\n\n`,
+    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
+)
